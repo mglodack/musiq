877aaff2da76e499d3284679e3a98c3fc0e03a65
Julio Garcia <juliogarcia93@gmail.com>
2014-04-12 14:45:58 -0700

    Initial Commit!

---
+++ Musiq/Controllers/APIControllers/MusiqAPIController.cs
@@ -0,0 +1,35 @@
+﻿using Jukebox.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Script.Services;
+
+namespace Musiq.Controllers
+{
+    [RoutePrefix("api")]
+    public class MusiqAPIController : ApiController
+    {
+        [Route("login/{username}/{password}")]
+        [HttpGet]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public async Task<object> AccountExists(string username, string password)
+        {
+            AccountRepository _context = new AccountRepository();
+            if (await _context.AccountExists(username, password))
+            {
+                var result = new { loginValid = true };
+                return result;
+            }
+            else
+            {
+                var result = new { loginValid = false };
+                return result;
+            }
+
+        }
+    }
+}
---
+++ Musiq/Repositories/AccountRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.Validation;
+using System.Diagnostics;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Musiq.Controllers;
+
+namespace Jukebox.Repositories
+{
+    public class AccountRepository
+    {
+        private IdentityDbContext _context { get; set; }
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public AccountRepository()
+        {
+            _context = new IdentityDbContext();
+            //Starts the links to the dbsets to call and go through them
+        }
+
+
+        public async Task<bool> AccountExists(string username, string password)
+        {
+            AccountController controller = new AccountController();
+            var user = await controller.UserManager.FindAsync(username,password);
+            if (user != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
