e1746d9a490f0c9bcb6729e8c77c6d9f62574d0d
Julio Garcia <juliogarcia93@gmail.com>
2014-04-16 17:52:49 -0700

    Syncing Visual Studio

---
+++ Musiq/Views/QueueController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Musiq.Views
+{
+    public class QueueController : Controller
+    {
+        //
+        // GET: /Queue/
+        public PartialViewResult Index()
+        {
+            return PartialView("_QueuePartial");
+        }
+	}
+}
